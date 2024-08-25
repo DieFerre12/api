fe4455049b84323ffa1f1bcb3337d87375b6837f
Diego <diferreira@uade.edu.ar>
2024-08-25 17:17:01 -0300

    arreglo palabra reservada sql

--- demo/src/main/java/com/uade/tpo/demo/entity/Order.java
+++ demo/src/main/java/com/uade/tpo/demo/entity/Order.java
@@ -11,11 +11,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data; 
 
 
 @Data
 @Entity
+@Table(name = "orders")
 public class Order  {
 
     @Id
