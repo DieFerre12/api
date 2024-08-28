ce76e040419497b1687cce51fefae8d9425ee4ae
DieFerre12 <164224192+DieFerre12@users.noreply.github.com>
2024-08-28 20:59:20 -0300

    Add files via upload

---
+++ api/demo/src/main/java/com/uade/tpo/demo/entity/Detail.java
@@ -0,0 +1,48 @@
+package com.uade.tpo.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+// Entidad `Detail` para el manejo de detalles de una orden
+@Data
+@Entity
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_facture;
+
+    @Column
+    private Long id_order;
+    @Column
+    private Long id_product;
+    @Column
+    private Integer quantity;
+    @Column
+    private Double price;
+    @Column
+    private int amount;
+
+
+
+    // Relación con la entidad `Order`
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+
+    // Relación con la entidad `Product`
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    // Relación con la entidad `Facture`
+    @ManyToOne
+    @JoinColumn(name = "facture_id", referencedColumnName = "id")
+    private Facture facture;
+}
