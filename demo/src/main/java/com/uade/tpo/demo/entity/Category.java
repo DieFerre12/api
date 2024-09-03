a6f27772f46bec54f3c18a0ba592e6880855c990
Joaco <joaquinoto23@gmail.com>
2024-09-03 16:14:32 -0300

    Category fix

--- demo/src/main/java/com/uade/tpo/demo/entity/Category.java
+++ demo/src/main/java/com/uade/tpo/demo/entity/Category.java
@@ -1,9 +1,30 @@
 package com.uade.tpo.demo.entity;
 
-public enum Category {
-    RUNNING,
-    BASKETBALL,
-    FOOTBALL,
-    STREET,
-    CASUAL
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Category {
+
+    public Category() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Description description;
+    
+
+    @OneToOne(mappedBy = "category")
+    private Product product;
 }
--- demo/src/main/java/com/uade/tpo/demo/service/category/CategoryService.java
+++ demo/src/main/java/com/uade/tpo/demo/service/category/CategoryService.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import com.uade.tpo.demo.entity.Category;
+import com.uade.tpo.demo.exceptions.CategoryDuplicateException;
 
 public interface CategoryService {
     public Page<Category> getCategories(PageRequest pageRequest);
 
     public Optional<Category> getCategoryById(Long categoryId);
+
+    public Category createCategory(String description) throws CategoryDuplicateException;
 
 }
