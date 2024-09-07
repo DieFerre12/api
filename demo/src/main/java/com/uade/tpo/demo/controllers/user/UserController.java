4a85205d5f9ff778bbf400c5a11381ba7a7c57f7
Joaco <joaquinoto23@gmail.com>
2024-09-07 13:53:24 -0300

    carrito

--- demo/src/main/java/com/uade/tpo/demo/controllers/user/UserController.java
+++ demo/src/main/java/com/uade/tpo/demo/controllers/user/UserController.java
@@ -1,12 +1,22 @@
 package com.uade.tpo.demo.controllers.user;
 
-import com.uade.tpo.demo.entity.User;
-import com.uade.tpo.demo.service.user.UserService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import com.uade.tpo.demo.entity.User;
+import com.uade.tpo.demo.service.shoppingCart.ShoppingCartService;
+import com.uade.tpo.demo.service.user.UserService;
 
 @RestController
 @RequestMapping("/users")
@@ -15,17 +25,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserRequest userRequest) {
         User newUser = userService.saveUser(
-                userRequest.getEmail(), 
-                userRequest.getName(), 
-                userRequest.getPassword(), 
-                userRequest.getFirstName(), 
-                userRequest.getLastName()
-        );
-        return ResponseEntity.ok(newUser);
-    }
+            userRequest.getEmail(),
+            userRequest.getName(),
+            userRequest.getPassword(),
+            userRequest.getFirstName(),
+            userRequest.getLastName()
+    );
+
+    // Crear carrito de compras vacío para el nuevo usuario
+    shoppingCartService.createCart(newUser.getId());
+
+    return ResponseEntity.ok(newUser);
+}
+
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
